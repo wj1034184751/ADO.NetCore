7fc95575ae29915e892ec4ccceb9192b4b2e5cf5
wj <1034184751@qq.com>
2019-05-03 22:18:01 +0800

    测试

--- Core1/WGCore/Test3/Controllers/HomeController.cs
+++ Core1/WGCore/Test3/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using BLL;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace Test3.Controllers
 {
@@ -52,12 +53,42 @@ namespace Test3.Controllers
             var result_2 = _userInfoBLL.GetBookCount();
             Tuple<int, int> resultTup = new Tuple<int, int>(result, result_2);
             _cache.Set<Tuple<int, int>>(cacheKey, resultTup);
+
+          
             return View();
             //return Content(string.Format("UserInfo:Count{0},Books:Count:{1}", result, result_2));
         }
 
+        public class DownLoadTest
+        {
+            Stopwatch watch = new Stopwatch();
+            public DownLoadTest()
+            {
+                watch.Start();
+            }
+            public async Task<Tuple<string, int>> DownLoadStringTaskAsync(string url)
+            {
+                Debug.WriteLine(string.Format("异步程序获取{0}开始运行:{1,4:N0}ms", url, watch.Elapsed.TotalMilliseconds));
+                WebClient wc = new WebClient();
+                string str = await wc.DownloadStringTaskAsync(url);
+                Debug.WriteLine(string.Format("异步程序获取{0}运行结束:{1,4:N0}ms", url, watch.Elapsed.TotalMilliseconds));
+                return new Tuple<string, int>(str, (int)watch.Elapsed.TotalSeconds);
+            }
+            public Tuple<string, int> DownLoadString(string url)
+            {
+                Debug.WriteLine(string.Format("异步程序获取{0}开始运行:{1,4:N0}ms", url, watch.Elapsed.TotalMilliseconds));
+                WebClient wc = new WebClient();
+                string str = wc.DownloadString(url);
+                Debug.WriteLine(string.Format("异步程序获取{0}运行结束:{1,4:N0}ms", url, watch.Elapsed.TotalMilliseconds));
+                return new Tuple<string, int>(str, (int)watch.Elapsed.TotalSeconds);
+            }
+        }
+
         public IActionResult About()
         {
+            DownLoadTest test = new DownLoadTest();
+            var item = test.DownLoadString("https://localhost:44379");
+            System.IO.File.WriteAllText("d:/wj1034184751.txt", item.Item1);
             var testCache = _cache.Get<Tuple<int, int>>(cacheKey);
             ViewData["Message"] = string.Format("Your application description page.t1:{0},t2:{1}", testCache.Item1, testCache.Item2);
             return View();
--- Core1/WGCore/Test3/Program.cs
+++ Core1/WGCore/Test3/Program.cs
@@ -15,22 +15,6 @@ namespace Test3
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
-            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            try
-            {
-                logger.Debug("init main");
-            }
-            catch (Exception ex)
-            {
-                //NLog: catch setup errors
-                logger.Error(ex, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                NLog.LogManager.Shutdown();
-            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
