73b1912a38b8c9ec5f0ac935d9029dbdf4e42964
wj <1034184751@qq.com>
2019-07-29 17:48:06 +0800

    home

---
+++ Core1/WGCore/Unity/PageResult.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unity
+{
+    public class BaseQueryParams
+    {
+        public BaseQueryParams()
+        {
+            this.pageNumber = 1;
+            this.pageSize = 20;
+            this.sort = "Id";
+            this.order = "desc";
+        }
+
+        /// <summary>
+        /// 页码
+        /// </summary>
+        public int pageNumber { get; set; }
+
+        /// <summary>
+        /// 分页数
+        /// </summary>
+        public int pageSize { get; set; }
+
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public string sort { get; set; }
+
+        private string _order;
+        /// <summary>
+        /// 正序倒序
+        /// </summary>
+        public string order
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(_order) ? "asc" : _order;
+            }
+            set
+            {
+                _order = value;
+            }
+        }
+
+        private string _keyword;
+        public string keyword
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(_keyword) ? null : $"%{_keyword}%";
+            }
+            set
+            {
+                _keyword = value;
+            }
+        }
+
+        public DateTime? startDate { get; set; }
+
+        public DateTime? endDate { get; set; }
+
+        public int skip
+        {
+            get
+            {
+                return (pageNumber - 1) * pageSize;
+            }
+        }
+    }
+}
---
+++ Core1/WGCore/Unity/ResultData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unity
+{
+    public class RestfulResult
+    {
+        public int Code { get; private set; }
+        public string Message { get; private set; }
+        public bool Success { get; private set; }
+        public long Timestamp { get; }
+        protected RestfulResult()
+        {
+            Timestamp = (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000;
+        }
+        public static RestfulResult Succeed(string message = "成功")
+        {
+            return new RestfulResult()
+            {
+                Code = 200,
+                Message = message,
+                Success = true
+            };
+        }
+        public static RestfulResult Fail(string message, int code = 500)
+        {
+            return new RestfulResult()
+            {
+                Code = code,
+                Message = message,
+                Success = false
+            };
+        }
+
+        public static RestfulResult<T> Fail<T>(T Result, string message, int code = 500)
+        {
+            return new RestfulResult<T>()
+            {
+                Code = code,
+                Message = message,
+                Result = Result,
+                Success = false
+            };
+        }
+
+        public static RestfulResult<T> Succeed<T>(T Result, string message = "成功")
+        {
+            return new RestfulResult<T>()
+            {
+                Code = 200,
+                Message = message,
+                Result = Result,
+                Success = true
+            };
+        }
+    }
+    public class RestfulResult<T> : RestfulResult
+    {
+        public RestfulResult() : base()
+        {
+        }
+        public virtual T Result { get; set; }
+    }
+}
